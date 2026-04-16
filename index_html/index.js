2ceff5e9703c12835ba97e6657ee310956d42f1e
laizadejesus <laizadejesus115@gmail.com>
2026-04-16 10:43:58 -0300

    finalizando

--- index_html/index.js
+++ index_html/index.js
@@ -3,3 +3,8 @@ function mudar_conteudo_elemento() {
     document.getElementById("h1").innerHTML= "hello javascript!";
     
 }
+
+function mudar_estilo(){
+
+document.getElementById("paragrafo").style.textAlign = "right";
+}
